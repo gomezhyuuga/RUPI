07a993f63f8012999a27d676b1bbf74f76783418
gomezhyuuga <gomezhyuuga@gmail.com>
2014-11-09 21:43:21 -0600

    Proyecto de sublime text

--- test.rb
+++ test.rb
@@ -1,8 +1,14 @@
-require 'data_mapper'
-DataMapper.setup(:default, 'mysql://root:@localhost/RUPI?encoding=UTF8')
-load "#{Dir.pwd}/models/models.rb"
-a = Registro.first
+# encoding: UTF-8
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper' # ORM
+require "#{Dir.pwd}/models/models.rb"
 
-DataMapper.repository(:default).adapter.resource_naming_convention = DataMapper::NamingConventions::Resource::Underscored
-
+# CONFIGS
+# Change public directory
+# set :public_folder, File.dirname(__FILE__) + '/static'
+# Change views directory
+# set :views, settings.root + '/templates'
+# A MySQL connection:
+DataMapper.setup(:default, 'mysql://root:@localhost/RUPI?charset=UTF-8')
 DataMapper.finalize
