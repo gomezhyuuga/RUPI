b1d4ccff1a2a383c67e7e71dd791553f3233727e
gomezhyuuga <gomezhyuuga@gmail.com>
2014-11-15 12:01:15 -0600

    Convertido a Modular Application

--- config.ru
+++ config.ru
@@ -1,1 +1,6 @@
-website.rb
+# encoding: UTF-8
+Encoding.default_external = 'UTF-8'
+Encoding.default_internal = 'UTF-8'
+
+require './website.rb'
+run RupiApp
--- website.rb
+++ website.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 # Extensions
 require 'sinatra/contrib'
 require 'data_mapper' # ORM
@@ -17,137 +17,140 @@ require "#{Dir.pwd}/models/models.rb"
 # set :public_folder, File.dirname(__FILE__) + '/static'
 # Change views directory
 # set :views, settings.root + '/templates'
-Encoding.default_external = 'UTF-8'
-Encoding.default_internal = 'UTF-8'
-enable :sessions
-set :session_secret, 'RUPI SECRET'
 
-# A MySQL connection:
-DataMapper.setup(:default, 'mysql://rupiapp:rupi82456@localhost/RUPI?encoding=UTF-8')
-DataMapper.finalize
+class RupiApp < Sinatra::Application
+	enable :sessions
+	set :session_secret, 'RUPI SECRET'
 
-# READ
-get '/' do
-	haml :index, layout: false
-end
-get '/animales/' do
-	@animales = Registro.all
-	haml :animales
-end
-get '/tratamientos/' do
-	@debug = session.inspect
-	@list = Tratamiento.all
-	tratamiento = @list.first
-	@detalles = DetalleTratamiento.all
-	haml :tratamientos
-end
-get '/animales/:folio?/?' do
-	@debug = params
-	@animal = Registro.get(params[:folio])
-	if @animal
-		haml :animal
-	else
-		"Not found"
+	# A MySQL connection:
+	DataMapper.setup(:default, 'mysql://rupiapp:rupi82456@localhost/RUPI?encoding=UTF-8')
+	DataMapper.finalize
+
+	# READ
+	get '/' do
+		haml :index, layout: false
 	end
-end
-
-# DELETE
-delete '/tratamientos/:id?/?' do
-	tratamiento = Tratamiento.get(params[:id])
-
-	begin
-		flash[:success] = "#{tratamiento.nombre} eliminado correctamente" if tratamiento.destroy
-	rescue Exception => e
-		flash[:danger] = "No se pudo eliminar #{tratamiento.nombre}"
-		flash[:details] = e.message
+	get '/animales/' do
+		@animales = Registro.all
+		haml :animales
 	end
-	redirect '/tratamientos/'
-end
-delete '/animales/tratamientos/:id' do
-	@t = DetalleTratamiento.get(params[:id])
-	begin
-		flash[:success] = "Tratamiento eliminado correctamente" if @t.destroy
-	rescue Exception => e
-		flash[:danger] = "No se pudo eliminar el tratamiento"
+	get '/tratamientos/' do
+		@debug = session.inspect
+		@list = Tratamiento.all
+		tratamiento = @list.first
+		@detalles = DetalleTratamiento.all
+		haml :tratamientos
 	end
-	redirect "/animales/#{@t.registro.id}"
-end
+	get '/animales/:folio?/?' do
+		@debug = params
+		@animal = Registro.get(params[:folio])
+		if @animal
+			haml :animal
+		else
+			"Not found"
+		end
+	end
 
- # CREATE
-get '/animales/tratamientos/new' do
-	@debug = session.inspect
-	@t = DetalleTratamiento.new
-	haml :animales_tratamiento
-end
-post '/animales/tratamientos/new' do
-	@t = DetalleTratamiento.new(params[:tratamiento])
+	# DELETE
+	delete '/tratamientos/:id?/?' do
+		tratamiento = Tratamiento.get(params[:id])
 
-	if @t.save
-		flash[:success] = "Tratamiento guardado correctamente"
-		redirect to "/animales/#{@t.folio}"
-	else
-		flash.now[:danger] = "Error guardando el tratamiento"
+		begin
+			flash[:success] = "#{tratamiento.nombre} eliminado correctamente" if tratamiento.destroy
+		rescue Exception => e
+			flash[:danger] = "No se pudo eliminar #{tratamiento.nombre}"
+			flash[:details] = e.message
+		end
+		redirect '/tratamientos/'
+	end
+	delete '/animales/tratamientos/:id' do
+		@t = DetalleTratamiento.get(params[:id])
+		begin
+			flash[:success] = "Tratamiento eliminado correctamente" if @t.destroy
+		rescue Exception => e
+			flash[:danger] = "No se pudo eliminar el tratamiento"
+		end
+		redirect "/animales/#{@t.registro.id}"
+	end
+
+	 # CREATE
+	get '/animales/tratamientos/new' do
+		@debug = session.inspect
+		@t = DetalleTratamiento.new
 		haml :animales_tratamiento
 	end
-end
-post '/tratamientos/new' do
-	@t = Tratamiento.new params[:tratamiento]
+	post '/animales/tratamientos/new' do
+		@t = DetalleTratamiento.new(params[:tratamiento])
 
-	if @t.save
-		flash[:success] = "Tratamiento guardado correctamente"
-		redirect to "/tratamientos/"
-	else
-		flash.now[:danger] = "Error guardando el tratamiento"
+		if @t.save
+			flash[:success] = "Tratamiento guardado correctamente"
+			redirect to "/animales/#{@t.folio}"
+		else
+			flash.now[:danger] = "Error guardando el tratamiento"
+			haml :animales_tratamiento
+		end
+	end
+	post '/tratamientos/new' do
+		@t = Tratamiento.new params[:tratamiento]
+
+		if @t.save
+			flash[:success] = "Tratamiento guardado correctamente"
+			redirect to "/tratamientos/"
+		else
+			flash.now[:danger] = "Error guardando el tratamiento"
+			haml :tratamiento
+		end
+	end
+
+	get '/tratamientos/new' do
+		@t = Tratamiento.new
 		haml :tratamiento
 	end
-end
-
-get '/tratamientos/new' do
-	@t = Tratamiento.new
-	haml :tratamiento
-end
-get '/tratamientos/:id/edit' do
-	@t = Tratamiento.get(params[:id])
-	haml :tratamiento_edit
-end
-
-# UPDATE
-put '/tratamientos/:id' do
-	@t = Tratamiento.get(params[:id])
-	if @t.update( params[:tratamiento] )
-		flash[:success] = "Tratamiento actualizado correctamente"
-		redirect to "/tratamientos/"
-	else
-		flash.now[:danger] = "No se pudo actualizar el tratamiento"
+	get '/tratamientos/:id/edit' do
+		@t = Tratamiento.get(params[:id])
 		haml :tratamiento_edit
 	end
-end
 
-# Helpers
-helpers do
-	def pdate(date)
-		date.strftime "%d/%m/%Y"
+	# UPDATE
+	put '/tratamientos/:id' do
+		@t = Tratamiento.get(params[:id])
+		if @t.update( params[:tratamiento] )
+			flash[:success] = "Tratamiento actualizado correctamente"
+			redirect to "/tratamientos/"
+		else
+			flash.now[:danger] = "No se pudo actualizar el tratamiento"
+			haml :tratamiento_edit
+		end
 	end
 
-	def action_buttons( edit = "", delete )
-		"<a class='btn btn-xs btn-link' href='#{edit}'>" +
-			"<i class='glyphicon glyphicon-pencil'></i>" +
-		"</button>" +
-		"<form class='inline' action='#{delete}' method='POST'>" +
-			"<input name='_method' type='hidden' value='delete' />" +
-			"<button class='btn btn-xs btn-link' type='submit'>" +
-				"<i class='glyphicon glyphicon-remove'></i>" +
+	# Helpers
+	helpers do
+		def pdate(date)
+			date.strftime "%d/%m/%Y"
+		end
+
+		def action_buttons( edit = "", delete )
+			"<a class='btn btn-xs btn-link' href='#{edit}'>" +
+				"<i class='glyphicon glyphicon-pencil'></i>" +
 			"</button>" +
-		"</form>"
+			"<form class='inline' action='#{delete}' method='POST'>" +
+				"<input name='_method' type='hidden' value='delete' />" +
+				"<button class='btn btn-xs btn-link' type='submit'>" +
+					"<i class='glyphicon glyphicon-remove'></i>" +
+				"</button>" +
+			"</form>"
+		end
+
+		def label_status( status, content )
+			# 1: Sin adoptar
+			# 2: Adoptado
+			# 3: Enfermo
+			dirs = [:success, :danger, :info]
+			"<span class='label label-#{dirs[status-1]}'>" +
+			content.to_s +
+			"</span>"
+		end
 	end
 
-	def label_status( status, content )
-		# 1: Sin adoptar
-		# 2: Adoptado
-		# 3: Enfermo
-		dirs = [:success, :danger, :info]
-		"<span class='label label-#{dirs[status-1]}'>" +
-		content.to_s +
-		"</span>"
-	end
+	run! if app_file == $0
 end
